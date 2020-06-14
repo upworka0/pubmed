a011173e502ecd0019d8526934257f2e68b0a650
upworka0 <upworka0@gmail.com>
2020-06-14 02:30:33 -0700

    fix: download excel file

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 var suggestion_url = "/suggestions?term=";
 var excel_file = "";
 var results = [];
+var table = null;
 
 // Async Ajax Request
 function AjaxRequest(url, method='GET', data=null){
@@ -32,7 +33,8 @@ function hide_spinner(){
 
 // Export excel file
 function export_excel(){
-    console.log("clicked!");
+    document.getElementById('_iframe').href = "/" + excel_file;
+    document.getElementById('_iframe').click();
 }
 
 // show detail of row
@@ -40,6 +42,28 @@ function show_detail(id){
     $('#modal').modal('show');
 }
 
+
+//truncate long text
+function truncate(str, len=40) {
+    /*
+        truncate text
+
+        @params:
+                n: string,
+                len: int
+        @return:
+                truncated string
+    */
+
+    if(str.length <= len) {
+        return str;
+    }
+
+    var ext = str.substring(str.length - 3, str.length);
+    var filename = str.replace(ext,'');
+
+    return filename.substr(0, len-3) + (str.length > len ? '...' : '');
+}
 
 
 /*$('#query').keypress(async function(eve){
@@ -61,10 +85,35 @@ function show_detail(id){
 
 // Populate the table data
 function populate_table(){
-    $('#results_table tbody').html('');
+    var html = "";
+    $('#table_div').empty();
+
+    var table = '<table id="results_table" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%"><thead><tr><th>_NO</th><th>Pubmed link</th><th>Title</th><th>Abstract</th><th>Authors</th><th>Author email</th><th>Author affiliation</th><th>PMCID</th><th>DOI</th><th>Full text link</th><th>Mesh terms</th><th>Publication type</th></tr></thead><tbody></tbody>';
+    $('#table_div').html(table);
+
+    for ( var i = 0 ; i < results.length; i++ ){
+        html += "<tr><td>" + (i+1) + "</td>";
+        html +="<td>" + results[i]["Pubmed link"] + "</td>";
+        html +="<td>" + results[i]["heading_title"] + "</td>";
+        html +="<td>" + truncate(results[i]["abstract"]) + "</td>";
+        html +="<td>" + truncate(results[i]["authors_list"]) + "</td>";
+        html +="<td>" + truncate(results[i]["affiliation"]) + "</td>";
+        html +="<td>" + results[i]["author_email"] + "</td>";
+        html +="<td>" + results[i]["pmcid"] + "</td>";
+        html +="<td>" + results[i]["doi"] + "</td>";
+        html +="<td>" + truncate(results[i]["full_text_links"]) + "</td>";
+        html +="<td>" + truncate(results[i]["mesh_terms"]) + "</td>";
+        html +="<td>" + truncate(results[i]["publication_types"]) + "</td>";
+        html += "</tr>";
+    }
+
+    console.log(html);
+    $('#results_table tbody').html(html);
+    $('#results_table').DataTable( { } );
+    $('#export_button').attr('disabled', false);
 }
 
-populate_table();
+
 // start scraping and show results in dataTable
 $('#submit').click(async function(eve){
     var keyword = $('#query').val();
